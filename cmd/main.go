782f7012192064b0dd367409e58b01963fd2364f
Tibart <tibart@biglebowski.nl>
2022-06-13 22:34:44 +0200

    added flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"biglebowski.nl/bullettime"
 )
 
 func main() {
-	// TODO: Add arguments
+	// Capture arguments
+	add := flag.NewFlagSet("add", flag.ExitOnError)
+	add_bd := add.String("bd", "", "Bullet description (mandatory)")
+	add_ref := add.String("ref", "", "External reference id")
+	add_d := add.String("d", time.Now().Format("2006-01-02"), "Bullet start date (yyyy-MM-dd)")
+	add_t := add.String("t", "", "Bullet meeting start time (hh:mm)")
+	add_m := add.Bool("m", false, "Bullet is an meeting")
+	add.Parse(os.Args[2:])
+	flag.Parse()
+
+	// Construct bullet list
+	b := bullettime.Bullets{}
+
+	// Interpreted flags
+	switch strings.ToUpper(os.Args[1]) {
+	case "ADD":
+		if *add_bd == "" {
+			fmt.Println("Flag -bd (bullet description) is mandatory!")
+			os.Exit(1)
+		}
+		d, err := time.ParseInLocation("2006-01-02", *add_d, time.Local)
+		if err != nil {
+			fmt.Printf("could not parse date string: %s\n", *add_d)
+			os.Exit(1)
+		}
+		if *add_m {
+			t, err := time.Parse("15:04", *add_t)
+			if err != nil {
+				fmt.Printf("could not parse time string: %s\n", *add_t)
+				os.Exit(1)
+			}
+			// BUG: Must be better way to get duration
+			dur, _ := time.ParseDuration(fmt.Sprintf("%vh%vm", t.Hour(), t.Minute()))
+			d = d.Add(dur)
+		}
+
+		if err = b.Add(*add_bd, *add_ref, *add_m, d); err != nil {
+			fmt.Println(err.Error())
+		}
+
+		//fmt.Println(*add_bd, *add_ref, *add_d, *add_t, *add_m)
+	}
+
+	//fmt.Println(*add_bd, *add_ref, *add_d, *add_t, *add_m)
 
 	///t := bullettime.NewBullet()
 	// now := time.Now()
-	b := bullettime.Bullets{}
+	//b := bullettime.Bullets{}
 	// b.Add("One", "", true, now)
 	// b.Add("Two", "", false, now)
 	// b.Add("Three", "", false, now)
@@ -25,14 +72,14 @@ func main() {
 	// b.Postpone(4)
 	// b.Cancel(5)
 
-	b.Load("./test.json")
+	//b.Load("./test.json")
 
 	fmt.Println(b.String())
 
 	fmt.Println(b.GetSchedule())
 
-	err := b.Save("./test.json")
+	//err := b.Save("./test.json")
 
-	fmt.Println(err)
+	//fmt.Println(err)
 
 }
