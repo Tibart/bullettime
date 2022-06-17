d7e471b9510f806861fa457924b00ebfd8dbe1f3
Tibart <tibart@biglebowski.nl>
2022-06-17 22:12:48 +0200

    Added add add and list flag

--- cmd/main.go
+++ cmd/main.go
@@ -22,17 +22,23 @@ var fc int
 var fm int
 var fp int
 var fr int
+var fl bool
 
 func init() {
-	flag.StringVar(&fa, "a", "", "Set bullet `task` description")
-	flag.StringVar(&fd, "d", time.Now().Format("2006-01-02"), "Set bullet start `date` (yyyy-MM-dd)")
-	flag.StringVar(&ft, "t", "", "Set bullet meeting start `time` (hh:mm)")
+	// Add flags
+	flag.StringVar(&fa, "a", "", "Set bullet `description`. Use quotation mark when description has more than one word.")
+	flag.StringVar(&fd, "d", time.Now().Format("2006-01-02"), "Set bullet start `date (yyyy-MM-dd)`")
+	flag.StringVar(&ft, "t", "", "Set bullet meeting start `time (hh:mm)`")
+	// Modifier flags
 	flag.IntVar(&fc, "c", -1, "Complete bullet `number` (mandatory")
 	flag.IntVar(&fm, "m", -1, "Move bullet `number` to next day")
 	flag.IntVar(&fp, "p", -1, "Postpone bullet `number`")
 	flag.IntVar(&fr, "r", -1, "Revoke bullet `number`")
+	// Presentation
+	flag.BoolVar(&fl, "l", false, "List all open bullets")
+
 	flag.Usage = func() {
-		fmt.Printf("Bullet time, version %v\nUsage of the program: \n", version)
+		fmt.Println("Usage of the program:")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -44,12 +50,23 @@ func main() {
 	b.Load(configPath)
 
 	//Set function flags
-	blt := bullettime.Bullet{}
 	if fa != "" {
+		if fa[0:1] == "-" {
+			fmt.Println("flag needs an argument: -a")
+			flag.Usage()
+			os.Exit(2)
+		}
+		if flag.NArg() > 0 {
+			fmt.Println("flag argument needs quotation marks: -a")
+			flag.Usage()
+			os.Exit(2)
+		}
+		blt := bullettime.Bullet{}
 		blt.Description = fa
 		d, err := time.ParseInLocation("2006-01-02", fd, time.Local)
 		if err != nil {
 			log.Printf("could not parse date string: %s\n", fd)
+			flag.Usage()
 			os.Exit(1)
 		}
 		blt.DateTime = d
@@ -68,76 +85,10 @@ func main() {
 		b.Add(blt)
 	}
 
-	fmt.Println(b.String())
-
-	// Interpreted flags
-	// switch strings.ToUpper(os.Args[1]) {
-	// case "ADD":
-	// 	if *add_b == "" {
-	// 		log.Println("flag -b (bullet description) is mandatory!")
-	// 		os.Exit(1)
-	// 	}
-	// 	d, err := time.ParseInLocation("2006-01-02", *add_d, time.Local)
-	// 	if err != nil {
-	// 		log.Printf("could not parse date string: %s\n", *add_d)
-	// 		os.Exit(1)
-	// 	}
-	// 	if *add_m {
-	// 		t, err := time.Parse("15:04", *add_t)
-	// 		if err != nil {
-	// 			log.Printf("could not parse time string: %s\n", *add_t)
-	// 			os.Exit(1)
-	// 		}
-	// 		// BUG: Must be better way to get duration
-	// 		dur, _ := time.ParseDuration(fmt.Sprintf("%vh%vm", t.Hour(), t.Minute()))
-	// 		d = d.Add(dur)
-	// 	}
-
-	// 	if err = b.Add(*add_b, *add_r, *add_m, d); err != nil {
-	// 		log.Println(err.Error())
-	// 	}
-	// case "LIST":
-	// 	fmt.Println(b.String())
-	// case "COMPLETE":
-	// 	i := *comp_n
-	// 	if i < 0 {
-	// 		log.Println("flag -n (bullet number) is mandatory!")
-	// 		os.Exit(1)
-	// 	}
-	// 	if i > len(b) {
-	// 		log.Println("bullet number does not exist")
-	// 		os.Exit(1)
-	// 	}
-
-	// 	b.Complete(i)
-	// }
-
-	//fmt.Println(*add_bd, *add_ref, *add_d, *add_t, *add_m)
-
-	///t := bullettime.NewBullet()
-	// now := time.Now()
-	//b := bullettime.Bullets{}
-	// b.Add("One", "", true, now)
-	// b.Add("Two", "", false, now)
-	// b.Add("Three", "", false, now)
-	// b.Add("Four", "", false, now)
-	// b.Add("Six", "", false, now)
-	// b.Add("Seven", "", false, now)
-
-	// b.Remove(2)
-	// b.Complete(2)
-	// b.Reschedule(3, 2)
-	// b.Postpone(4)
-	// b.Cancel(5)
-
-	//b.Load("./test.json")
-
-	//fmt.Println(b.String())
+	// List bullet journal
+	if fl {
+		fmt.Println(b.String())
+	}
 
 	b.Save(configPath)
-
-	//err := b.Save("./test.json")
-
-	//fmt.Println(err)
-
 }
